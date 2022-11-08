56a1c3e4913e247cb404e48f49d9ce593f278a76
Nikolay Nikolov <nikolaizhnikolov@gmail.com>
2022-11-08 14:02:14 +0200

    Make blocks change size on grid resize.

--- etch/etch.js
+++ etch/etch.js
@@ -1,7 +1,12 @@
 const content = document.querySelector(".content");
 const resizeButton = document.querySelector(".resizeButton");
 
+const CONTENT_WIDTH = 640;
+const CONTENT_HEIGHT = 480;
+
 const OPACITY_STEP = 0.2;
+
+const PX = "px";
 
 resizeButton.addEventListener('click', () => {
     resizeGrid(prompt("New Grid Size:"));
@@ -17,14 +22,16 @@ function resizeGrid(gridSize) {
 function createBlocks(gridSize) {
     for (let i = 0; i < gridSize; i++) {
         for (let j = 0; j < gridSize; j++) {
-            content.appendChild(createBlock());
+            content.appendChild(createBlock(gridSize));
         }
     }
 }
 
-function createBlock() {
+function createBlock(gridSize) {
     const div = document.createElement("div");
     div.classList.add("block");
+    div.style.width = CONTENT_WIDTH / gridSize + PX;
+    div.style.height = CONTENT_HEIGHT / gridSize + PX;
     div.addEventListener('mouseenter', () => {
         div.style.opacity = Number(div.style.opacity) + OPACITY_STEP;
     });
@@ -37,4 +44,4 @@ function removeBlocks() {
     }
 }
 
-resizeGrid(16);
+resizeGrid(2);
