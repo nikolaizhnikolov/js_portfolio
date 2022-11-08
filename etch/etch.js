037a9d0589ba1212063834376cc743b7f71615df
Nikolay Nikolov <nikolaizhnikolov@gmail.com>
2022-11-08 19:27:07 +0200

    Redo interface and add slider.

--- etch/etch.js
+++ etch/etch.js
@@ -1,10 +1,8 @@
 const content = document.querySelector(".content");
 
 const DEFAULT_GRID_SIZE = 16; 
-
 const CONTENT_WIDTH = 640;
 const CONTENT_HEIGHT = 480;
-
 const OPACITY_STEP = 0.2;
 
 const PX = "px";
@@ -12,18 +10,20 @@ const PX = "px";
 let mousePressed = false;
 
 function init(){ 
-    const resizeButton = document.querySelector(".resizeButton");    
-    resizeButton.addEventListener('click', () => {
-        resizeGrid(prompt("New Grid Size:"));
-    })
-
     const body = document.querySelector("body");
     body.addEventListener('mousedown', () => mousePressed = true);
     body.addEventListener('mouseup', () => mousePressed = false);
 
+    const slider = document.querySelector(".slider_input");  
+    const label = document.querySelector(".slider_label");
+
+    slider.addEventListener('input', () => {
+        label.innerHTML = `${slider.value}x${slider.value}`;
+    });
+    slider.addEventListener('change', () => resizeGrid(slider.value));
+
     resizeGrid(DEFAULT_GRID_SIZE);
 }
-init();
 
 function resizeGrid(gridSize) {
     if(gridSize === null) return;
@@ -40,16 +40,21 @@ function createBlocks(gridSize) {
     }
 }
 
+function increaseElementOpacity() {
+    this.style.opacity = Number(this.style.opacity) + OPACITY_STEP;
+}
+
 function createBlock(gridSize) {
     const div = document.createElement("div");
     div.classList.add("block");
     div.style.width = CONTENT_WIDTH / gridSize + PX;
     div.style.height = CONTENT_HEIGHT / gridSize + PX;
+
+    div.addEventListener('mousedown', () => increaseElementOpacity);
     div.addEventListener('mouseover', () => {
-        if(mousePressed) {
-            div.style.opacity = Number(div.style.opacity) + OPACITY_STEP;
-        }        
+        if(mousePressed) div.style.opacity = Number(div.style.opacity) + OPACITY_STEP;
     });
+
     return div;
 }
 
