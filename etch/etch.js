97f39fde842504b27bad483150d6c9d4cecfb387
Nikolay Nikolov <nikolaizhnikolov@gmail.com>
2022-11-08 11:30:39 +0200

    Change background color when hovering over a block

--- etch/etch.js
+++ etch/etch.js
@@ -1,15 +1,18 @@
 const content = document.querySelector(".content");
 const GRID_SIZE = 16;
 
+function paint(event) {
+    console.log(event.srcElement.classList.add("block__hover"));
+}
+
 function createBlocks() {
 
     for (let i = 0; i < GRID_SIZE; i++) {
         for (let j = 0; j < GRID_SIZE; j++) {
             const div = document.createElement("div");
-            div.classList.add("block");
             div.textContent = GRID_SIZE * i + j + 1;
-            // div.style.gridRow = i+1;
-            // div.style.gridColumn = j+1;
+            div.classList.add("block");
+            div.addEventListener('mouseenter', paint);
             content.appendChild(div);
         }
     }
