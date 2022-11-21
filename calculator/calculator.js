3ba79b746143c7839f1fa7b62f591baaedd74a3c
Nikolay Nikolov <nikolaizhnikolov@gmail.com>
2022-11-21 17:46:38 +0200

    Separate operators and add logic for zero

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,61 +1,43 @@
-const add       = (a, b) => a + b;
-const subtract  = (a, b) => a - b;
-const multiply  = (a, b) => a * b;
-const divide    = (a, b) => a / b;
-const remainder = (a, b) => a % b;
-
-const ADD = 0;
-const SUBTRACT = 1;
-const MULTIPLY = 2;
-const DIVIDE = 3;
-const REMAINDER = 4;
-
-const OPERATORS =  {
-    0 : add,
-    1 : subtract,
-    2 : multiply,
-    3 : divide,
-    4 : remainder
-}
-
-const operate = function (operator, a, b) {
-    if(!OPERATORS[operator]) {
-        throw TypeError("No such operator exists!");
-    }
-
-    if (typeof a !== "number" || typeof b !== "number") {
-        throw TypeError("Operation Arguments Must be Numeric!");
-    }
-    
-    return OPERATORS[operator](a, b);
-}
+const displayHistory = document.querySelector(".display__history");
+const displayResult = document.querySelector(".display__result");
 
 let history = "";
 let result = "";
 
+const appendHistory = (value) => {
+    history += value;
+    displayHistory.textContent = history;
+}
+
+const setHistory = (value) => {
+    history = String(value);
+    displayHistory.textContent = history;
+}
+
+const setResult = (value) => {
+    result = String(value);
+    displayResult.textContent = result;
+}
+
 (function() {
-    const displayHistory = document.querySelector(".display__history");
-    const displayResult = document.querySelector(".display__result");
+    document.querySelector(".clear").addEventListener("click", () => {
+        setHistory(0);
+        setResult(0);
+    });
 
-    const updateResult = (value) => {
-        result += value;
-        displayResult.textContent = result;
-    }
-
-    const clearResult = () => {
-        result = "";
-        displayResult.textContent = result;
-    }
-
-    const clear = document.querySelector(".clear");
-    clear.addEventListener("click", () => clearResult());
-
-    const numbers = document.querySelectorAll(".number");
-    numbers.forEach((n) => {
+    document.querySelectorAll(".number").forEach((n) => {
         n.addEventListener("click", () => {
-            updateResult(n.dataset.value);
+            if(Number(history) === 0)
+                setHistory(n.dataset.value);
+            else
+                appendHistory(n.dataset.value);
         })
     });
+
+    document.querySelector(".zero").addEventListener("click", () => {
+        if(Number(history) !== 0 && !(history.includes(".")))
+            appendHistory(0);
+    })
 
     // const decimalPoint ...
 
