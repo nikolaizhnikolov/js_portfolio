8b0b4636bb56d64a0dbe399c86d73416e4f6cac0
Nikolay Nikolov <nikolaizhnikolov@gmail.com>
2022-11-24 14:44:32 +0200

    Add type check to clamp

--- util.js
+++ util.js
@@ -1,5 +1,12 @@
 const util = (function() {
     const clamp = function(value, min, max) {
+        value = Number(value);
+        min   = Number(min);
+        max   = Number(max);
+
+        if(isNaN(value) || isNaN(min) || isNaN(max))
+            throw TypeError("Expected values convertable to Numeric!");
+            
         return Math.min(max, Math.max(min,value));
     }
 
