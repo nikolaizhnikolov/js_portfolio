f1e076d994f2e3d03222fa378acbaddbb46dad4b
Nikolay Nikolov <nikolaizhnikolov@gmail.com>
2022-12-30 13:22:07 +0200

    Add remove functionality on books

--- library/library.js
+++ library/library.js
@@ -18,63 +18,72 @@ You will need to associate your DOM elements with the
 
 */
 
-let lib = [];
+const library = (function () {
+    let lib = [];
 
-const Book = function (title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-};
+    const Book = function (title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    };
 
-const addBookToLibrary = function (title, author, pages, read) {
-    const book = new Book(title, author, pages, read);
-    lib.push(book);
-};
+    const addBookToLibrary = function (title, author, pages, read) {
+        const book = new Book(title, author, pages, read);
+        lib.push(book);
+    };
 
-// TODO
-// const removeBookFromLibrary = function(title) {
-// for each book - if title matches remove
-// return true
-// }
+    const removeBookFromLibrary = function (book) {
+        const books = document.querySelector(".books");
+        books.removeChild(book);
+        lib.forEach((b) => {
+            const title = book.getElementsByClassName("book__title")[0].textContent;
+            if ( b.title === title) {
+                const index = lib.indexOf(b);
+                lib.splice(index, 1);
+                return;
+            }
+        });
+    };
 
-const createDiv = (divClass, textContent) => {
-    const div = document.createElement("div");
-    div.classList.add(divClass);
-    div.textContent = textContent;
-    return div;
-};
+    const createDiv = (divClass, textContent) => {
+        const div = document.createElement("div");
+        div.classList.add(divClass);
+        div.textContent = textContent;
+        return div;
+    };
 
-const createRemoveButton = function (title) {
-    const button = document.createElement("button");
-    button.classList.add("removeBookButton");
-    //TODO:
-    // button.addEventListener("click", (title) => {});
-    // remove from lib, if returned true find and remove child
-    button.textContent = "Remove";
-    return button;
-};
+    const createRemoveButton = function (title) {
+        const button = document.createElement("button");
+        button.classList.add("removeBookButton");
+        button.addEventListener("click", () => {
+            removeBookFromLibrary(button.parentNode);
+        });
+        button.textContent = "Remove";
+        return button;
+    };
 
-const addBookCard = function (book) {
-    const bookCard = createDiv("book");
-    bookCard.appendChild(createDiv("book__title", book.title));
-    bookCard.appendChild(createDiv("book__author", book.author));
-    bookCard.appendChild(createDiv("book__pages", book.pages));
-    if (book.read) {
-        bookCard.classList.add("read");
-    }
-    bookCard.appendChild(createRemoveButton(book.title));
+    const addBookCard = function (book) {
+        const bookCard = createDiv("book");
+        bookCard.appendChild(createDiv("book__title", book.title));
+        bookCard.appendChild(createDiv("book__author", book.author));
+        bookCard.appendChild(createDiv("book__pages", book.pages));
+        if (book.read) {
+            bookCard.classList.add("read");
+        }
+        bookCard.appendChild(createRemoveButton(book.title));
 
-    const addBookButton = document.querySelector(".addBookButton");
-    const books = addBookButton.parentNode;
-    books.insertBefore(bookCard, addBookButton);
-};
+        const addBookButton = document.querySelector(".addBookButton");
+        const books = addBookButton.parentNode;
+        books.insertBefore(bookCard, addBookButton);
+    };
 
-(function init() {
-    addBookToLibrary("test", "test", 1, false);
-    addBookToLibrary("Guards! Guards!", "Terry Pratchett", 324, true);
-    addBookToLibrary("The Light Fantastic", "Terry Pratchett", 256, true);
-    addBookToLibrary("The Hogfather", "Terry Pratchett", 286, true);
+    (function init() {
+        addBookToLibrary("test", "test", 1, false);
+        addBookToLibrary("Guards! Guards!", "Terry Pratchett", 324, true);
+        addBookToLibrary("The Light Fantastic", "Terry Pratchett", 256, true);
+        addBookToLibrary("The Hogfather", "Terry Pratchett", 286, true);
 
-    lib.forEach((book) => addBookCard(book));
+        lib.forEach((book) => addBookCard(book));
+    })();
 })();
